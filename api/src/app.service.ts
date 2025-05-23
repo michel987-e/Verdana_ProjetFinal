7ef5e97650a866580fd53f4f44c4f4b18e064822
bikoum_s <bikoum_s@etna-alternance.net>
2025-05-23 15:38:01 +0200

    baseduback

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'bienvenue sur le backend Verdana';
+    return 'bienvenue sur le backend verdana';
   }
 }
--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app =await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
   console.log(`🚀 Server running on http://localhost:${process.env.PORT || 3000}`);
