24e7476a555852600abba32851795f72b2cf0083
michel_e <michel_e@etna-alternance.net>
2025-06-16 15:20:47 +0200

    page des capteurs de la plante v1

--- Verdana/App.tsx
+++ Verdana/App.tsx
@@ -2,8 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Plante from './Plante';
-
+import Plante from './plante3';
+import Profil from './Profil';
+import Deconnexion from './Deconnexion';
+import Securite from './Securite';
+import Gestion from './Gestion';
+import Notifications from './Notifications';
+import Support from './Support';
 const Stack = createStackNavigator();
 
 function HomeScreen({ navigation }: { navigation: any }) {
@@ -12,8 +17,9 @@ function HomeScreen({ navigation }: { navigation: any }) {
 <Text>Open up App.tsx to start working on your app!</Text>
       <View style={styles.buttonRow}>
         <Button title="Connexion" onPress={() => alert('abababbaba')} />
-        <Button title="Plante" onPress={() => navigation.navigate('Plante')} />
+        <Button title="Plante" onPress={() => navigation.navigate('plante3')} />
         <Button title="Support" onPress={() => alert('bahahaha !')} />
+          
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,7 +31,12 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Plante" component={Plante} />
+        <Stack.Screen name="plante3" component={Plante} />
+        <Stack.Screen name="Profil" component={Profil} />
+        <Stack.Screen name="Securite" component={Securite} />
+        <Stack.Screen name="Gestion" component={Gestion} />
+        <Stack.Screen name="Notifications" component={Notifications} />
+        <Stack.Screen name="Support" component={Support} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Verdana/Gestion.tsx
@@ -0,0 +1,11 @@
+// Profil.tsx minimal pour navigation
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Gestion() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Gestion</Text>
+    </View>
+  );
+}
