d0d4e3124bcf3f99f838e21d9fe099a36080f771
semavo_t <semavo_t@etna-alternance.net>
2025-06-27 11:52:25 +0200

    [ADD] Cookie Token is working git status Added new route (logout, validate -> check if token valid)

--- user-service/src/auth/jwt-auth.guard.ts
+++ user-service/src/auth/jwt-auth.guard.ts
@@ -1,5 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard implements CanActivate {
+    constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = request.cookies?.auth_token;
+    if (!token) {
+        throw new UnauthorizedException();
+    }
+    try {
+        const payload = await this.jwtService.verifyAsync(
+            token,
+            {
+                secret: process.env.JWT_SECRET || 'dev_secret'
+            }
+        );
+        request.user = payload
+    } catch {
+        throw new UnauthorizedException();
+    }
+    return true;
+  }
+}
