6042420b49dc640e0f81c31e385ae4e0997580e8
semavo_t <semavo_t@etna-alternance.net>
2025-06-27 12:45:05 +0200

    [ADD] Middleware to chatbot and flower-service, route protected with cookie and auth_token.

--- user-service/src/auth/jwt-auth.guard.ts
+++ user-service/src/auth/jwt-auth.guard.ts
@@ -6,23 +6,23 @@ import { Request } from 'express';
 export class JwtAuthGuard implements CanActivate {
     constructor(private jwtService: JwtService) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest<Request>();
-    const token = request.cookies?.auth_token;
-    if (!token) {
-        throw new UnauthorizedException();
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest<Request>();
+        const token = request.cookies?.auth_token;
+        if (!token) {
+            throw new UnauthorizedException();
+        }
+        try {
+            const payload = await this.jwtService.verifyAsync(
+                token,
+                {
+                    secret: process.env.JWT_SECRET || 'dev_secret'
+                }
+            );
+            request.user = payload
+        } catch {
+            throw new UnauthorizedException();
+        }
+        return true;
     }
-    try {
-        const payload = await this.jwtService.verifyAsync(
-            token,
-            {
-                secret: process.env.JWT_SECRET || 'dev_secret'
-            }
-        );
-        request.user = payload
-    } catch {
-        throw new UnauthorizedException();
-    }
-    return true;
-  }
 }
