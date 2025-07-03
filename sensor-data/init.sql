cf23529a00e4e8c800d017d791b9313af8022607
semavo_t <semavo_t@etna-alternance.net>
2025-07-03 10:06:03 +0200

    [DEL] remove sensor-data for now

--- sensor-data/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS sensor_data (
-  id SERIAL PRIMARY KEY,
-  pot_id INTEGER NOT NULL,
-  temperature FLOAT,
-  humidity FLOAT,
-  soil FLOAT,
-  light FLOAT,
-  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
