d30a13cbd0d871a78ff5dc039e8efe90339c49fe
semavo_t <semavo_t@etna-alternance.net>
2025-07-03 10:44:16 +0200

    [FIX] quick fix -> name of service / indent + volume in dockercompose

--- sensor/src/app.service.ts
+++ sensor/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Sensor-service';
+  getService(): string {
+    return 'Sensor-Service';
   }
 }
